dcc0269f74649cdad2488f5ebfd3d79d0c054e15
srinivas <dharamcnu@gmail.com>
2024-08-21 20:41:40 +0530

    first commit

--- list-users.sh
+++ list-users.sh
@@ -37,6 +37,5 @@ function list_users_with_read_access {
 }
 
 # Main script
-
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
 list_users_with_read_access  # Call the function to list users with read access
